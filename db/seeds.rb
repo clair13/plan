f9a0cc346ad59a9d4b81cfb9ca3ee04dc27b92ec
clair13 <theclairvoyant1313@yahoo.gr>
2017-08-27 18:10:31 +0300

    Generate plan model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+plans = Plan.create(
+	[
+		{
+			title: "A new car design",
+			body: "Made of aluminium"
+		},
+		{
+			title: "A twitter client",
+			body:  "Only for replying to mentions and direct messages"
+		},
+		{
+			title: "A film based on nfl",
+			body:  "A drama based on deaths by concussions"
+		},
+		{
+			title: "Board game",
+			body: "Like monopoly but more complicated"
+		}
+	])
